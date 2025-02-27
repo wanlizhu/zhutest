c8037ad5230741eb29719ffdeca02be554cce64a
wanlizhu <wanlizhu@hotmail.com>
2025-02-26 17:44:43 -0800

    s

--- utils.sh
+++ utils.sh
@@ -43,6 +43,15 @@ function zhu-connect-nvidia-vpn {
         eval $(openconnect --useragent="AnyConnect-compatible OpenConnect VPN Agent" --external-browser $(which google-chrome) --authenticate ngvpn02.vpn.nvidia.com/SAML-EXT)
     fi
 
+    if [[ ! -z $(pidof openconnect) ]]; then
+        read -e -i yes -p "Kill previous openconnect process ($(pidof openconnect))? " ans
+        if [[ $ans == yes ]]; then
+            sudo kill -SIGINT $(pidof openconnect)
+            echo "Wait for 3 seconds after killing openconnect"
+            sleep 3
+        fi
+    fi
+
     [ -n ["$COOKIE"] ] && echo -n "$COOKIE" | sudo openconnect --cookie-on-stdin $CONNECT_URL --servercert $FINGERPRINT --resolve $RESOLVE 
 }
 
