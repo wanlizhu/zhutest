c749174fc5adfd04548f4a2d4939fab206c25074
wanlizhu <wanlizhu@hotmail.com>
2025-03-10 10:46:57 -0700

    Mon Mar 10 10:46:57 PDT 2025

--- utils.sh
+++ utils.sh
@@ -1209,8 +1209,8 @@ function zhu-fex-sudo {
     if [[ ! -z $program && -e $rootfs$program ]]; then
         sudo FEX_ROOTFS=$rootfs FEXInterpreter $rootfs$program "$@"
     elif [[ ! -z $program && ! "$program" =~ "/" ]]; then
-        program=$(find $rootfs -type f -name $program)
-        sudo FEX_ROOTFS=$rootfs FEXInterpreter $rootfs$program "$@"
+        program=$(find $rootfs/usr/bin -type f -executable -name $program)
+        sudo FEX_ROOTFS=$rootfs FEXInterpreter $program "$@"
     else
         echo "$program doesn't exist in $rootfs"
         return -1
