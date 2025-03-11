bddc84566b101a1c9234c0d3e42d07222eb32fb7
Wanli Zhu <zhu.wanli@icloud.com>
2025-03-11 16:06:09 -0700

    Tue Mar 11 04:06:09 PM PDT 2025

--- utils.sh
+++ utils.sh
@@ -1314,24 +1314,9 @@ function zhu-chroot-in-fex {
     rootfs="$HOME/.fex-emu/RootFS/$ubuntu"
 
     pushd $rootfs >/dev/null 
-    if [[ -e ./chroot.py && -e ~/zhutest/src/chroot.py ]]; then
-        if ! cmp -s ./chroot.py ~/zhutest/src/chroot.py; then
-            read -e -i yes -p "Use ~/zhutest/src/chroot.py? (yes/no): " ans
-            if [[ $ans == yes ]]; then
-                rm -rf ./chroot.py  
-                cp -f  ~/zhutest/src/chroot.py .
-                chmod +x ./chroot.py
-            fi
-        fi
-    fi
-    if [[ ! -e ./chroot.py ]]; then
-        if [[ -e ~/zhutest/src/chroot.py ]]; then
-            cp ~/zhutest/src/chroot.py .
-        else
-            wget https://raw.githubusercontent.com/FEX-Emu/RootFS/refs/heads/main/Scripts/chroot.py 
-        fi 
-        chmod +x ./chroot.py 
-    fi
+
+    wget https://raw.githubusercontent.com/FEX-Emu/RootFS/refs/heads/main/Scripts/chroot.py 
+    chmod +x ./chroot.py 
 
     if [[ -z $(which patchelf) ]]; then
         sudo apt install -y patchelf
