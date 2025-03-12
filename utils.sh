7614cd9c373565d15385ec15fdca6fb2e455d154
Wanli Zhu <zhu.wanli@icloud.com>
2025-03-12 15:18:29 -0700

    Wed Mar 12 03:18:29 PM PDT 2025

--- utils.sh
+++ utils.sh
@@ -2063,50 +2063,53 @@ function zhu-gtlfs-download {
     popd >/dev/null 
 }
 
+function zhu-append-and-export {
+    local name=$1
+    local value=$2
+    if [[ -z ${!name} ]]; then
+        eval "export $name=$value"
+    else
+        if [[ "${!name}" != *"$value"* ]]; then
+            eval "export $name=$value:${!name}"
+        fi  
+    fi
+    echo "export $name=${!name}"
+}
+
 function zhu-vulkan-api-capture {
     if [[ ! -e ~/gfxreconstruct.git/build/linux/x64/output/bin/gfxrecon-capture-vulkan.py ]]; then
         if [[ ! -d ~/gfxreconstruct.git ]]; then
             git clone --recursive https://github.com/LunarG/gfxreconstruct.git ~/gfxreconstruct.git 
         fi
 
-        arch=x64 
         sudo apt install -y git cmake build-essential libx11-xcb-dev libxcb-keysyms1-dev libwayland-dev libxrandr-dev zlib1g-dev liblz4-dev libzstd-dev
         sudo apt install -y g++-multilib libx11-xcb-dev:i386 libxcb-keysyms1-dev:i386 libwayland-dev:i386 libxrandr-dev:i386 zlib1g-dev:i386 liblz4-dev:i386 libzstd-dev:i386
-        if [[ $(uname -m) == aarch64 ]]; then
-            sudo apt install -y g++-aarch64-linux-gnu
-            arch=arm64
-        fi 
 
         pushd . >/dev/null 
         cd ~/gfxreconstruct.git 
         git submodule update --init
-        python3 scripts/build.py --arch $arch --config release --parallel $(nproc) --skip-check-code-style --skip-tests --skip-d3d12-support 
-        echo "gfxreconstruct is compiled! run again."
+        python3 scripts/build.py --arch x64 --config release --parallel $(nproc) --skip-check-code-style --skip-tests --skip-d3d12-support 
+        echo "gfxreconstruct is compiled!"
         popd >/dev/null
-        return 
     fi 
 
-    echo "These environment variables are exported!"
-    if [[ -z $VK_LAYER_SETTINGS_PATH ]]; then
-        export VK_LAYER_SETTINGS_PATH=$HOME/gfxreconstruct.git/build/linux/x64/output/share/vulkan/explicit_layer.d/VkLayer_gfxreconstruct.json 
-    else
-        export VK_LAYER_SETTINGS_PATH=$HOME/gfxreconstruct.git/build/linux/x64/output/share/vulkan/explicit_layer.d/VkLayer_gfxreconstruct.json:$VK_LAYER_SETTINGS_PATH 
-    fi
-    echo "export VK_LAYER_SETTINGS_PATH=$VK_LAYER_SETTINGS_PATH"
+    zhu-append-and-export VK_INSTANCE_LAYERS VK_LAYER_LUNARG_gfxreconstruct
+    zhu-append-and-export VK_LAYER_PATH $HOME/gfxreconstruct.git/build/linux/x64/output/share/vulkan/explicit_layer.d
 
-    if [[ -z $VK_LAYER_PATH ]]; then
-        export VK_LAYER_PATH=$HOME/gfxreconstruct.git/build/linux/x64/output/lib/libVkLayer_gfxreconstruct.so
-    else
-        export VK_LAYER_PATH=$HOME/gfxreconstruct.git/build/linux/x64/output/lib/libVkLayer_gfxreconstruct.so:$VK_LAYER_PATH 
-    fi
-    echo "export VK_LAYER_PATH=$VK_LAYER_PATH"
+    library_path=$(jq -r '.layer.library_path' $HOME/gfxreconstruct.git/build/linux/x64/output/share/vulkan/explicit_layer.d/VkLayer_gfxreconstruct.json)
+    if [[ $library_path == "/"* ]]; then
+        echo "VkLayer_gfxreconstruct.json->layer->library_path is NOT an absolute path of libVkLayer_gfxreconstruct.so!"
+        echo "[1] Edit VkLayer_gfxreconstruct.json to use absolute path"
+        echo "[2] Edit LD_LIBRARY_PATH to add parent directory"
+        read -p "Select: " selection
 
-    if [[ -z $VK_INSTANCE_LAYERS ]]; then
-        export VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_gfxreconstruct
-    else
-        export VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_gfxreconstruct:$VK_INSTANCE_LAYERS 
+        if [[ $selection == 1 ]]; then
+            jq --arg real_path "/home/wanliz/gfxreconstruct.git/build/linux/x64/output/lib/libVkLayer_gfxreconstruct.so" '.layer.library_path = $real_path' -i $HOME/gfxreconstruct.git/build/linux/x64/output/share/vulkan/explicit_layer.d/VkLayer_gfxreconstruct.json
+            echo "Replaced VkLayer_gfxreconstruct.json->layer->library_path with its real path"
+        else
+            zhu-append-and-export LD_LIBRARY_PATH $HOME/gfxreconstruct.git/build/linux/x64/output/lib
+        fi
     fi
-    echo "export VK_INSTANCE_LAYERS=$VK_INSTANCE_LAYERS"
 }
 
 function zhu-test-quake2rtx {
