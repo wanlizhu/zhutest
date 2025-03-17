8b38132c9bf9967d9358fcce3cf435b9bc3b1ed9
wanlizhu <wanlizhu@hotmail.com>
2025-03-17 11:54:31 -0700

    Mon Mar 17 11:54:31 PDT 2025

--- zzz.sh
+++ zzz.sh
@@ -1,5 +1,6 @@
 source ~/zhutest/utils.sh
 zhu-test-viewperf-maya-subtest5 &
 sleep 1
-zhu-cpu-utilization
+rm -rf /tmp/cpu-utilization.log
+zhu-cpu-utilization $(pidof viewperf)
 cat /tmp/cpu-utilization.log
