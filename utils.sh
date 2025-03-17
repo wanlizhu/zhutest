3136f8c141352511a8e575f15f3e884ce38c7e68
wanlizhu <wanlizhu@hotmail.com>
2025-03-16 17:48:56 -0700

    Sun Mar 16 17:48:56 PDT 2025

--- utils.sh
+++ utils.sh
@@ -300,10 +300,15 @@ function zhu-generate-flamegraph {
     sudo ~/flamegraph.git/stackcollapse-recursive.pl /tmp/$(basename $1).script.collapse > $(basename $1).folded && echo "Generated $(basename $1).folded" &&
     sort -k2 -nr $(basename $1).folded | head -n 1000 > $(basename $1).head1000.folded && echo "Generated $(basename $1).head1000.folded" &&
     sudo ~/flamegraph.git/flamegraph.pl $(basename $1).folded > $(basename $1).svg  && echo "Generated $(basename $1).svg" &&
-    sudo ~/flamegraph.git/flamegraph.pl --minwidth '1%' $(basename $1).folded > $(basename $1).minwidth1.svg  && echo "Generated $(basename $1).minwidth1.svg" &&
-    echo "[optional] Generating a text-based graph (this may take time). Press [CTRL-C] to cancel." && sudo perf report --stdio --show-nr-samples --show-cpu-utilization --threads --input=$(basename $1) > /tmp/$(basename $1).graph.txt &&
-    mv /tmp/$(basename $1).graph.txt $(basename $1).graph.txt &&
-    echo "Generated $(basename $1).graph.txt"
+    sudo ~/flamegraph.git/flamegraph.pl --minwidth '1%' $(basename $1).folded > $(basename $1).minwidth1.svg  && echo "Generated $(basename $1).minwidth1.svg" 
+    if [[ -e $(basename $1).svg ]]; then
+        read -e -i no -p "Generate a text-based graph (this may take time)? (yes/no): " ans
+        if [[ $ans == yes ]]; then
+            sudo perf report --stdio --show-nr-samples --show-cpu-utilization --threads --input=$(basename $1) > /tmp/$(basename $1).graph.txt &&
+            mv /tmp/$(basename $1).graph.txt $(basename $1).graph.txt &&
+            echo "Generated $(basename $1).graph.txt"
+        fi 
+    fi 
     popd >/dev/null 
 }
 
