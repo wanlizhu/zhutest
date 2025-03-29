7215274356b6edd64174b713942f0001d4d1d8ce
wanlizhu <wanlizhu@hotmail.com>
2025-03-29 01:42:05 -0700

    Sat Mar 29 01:42:05 PDT 2025

--- nvbugs/gdm3-perf-overhead.sh
+++ nvbugs/gdm3-perf-overhead.sh
@@ -1,14 +1,21 @@
 function zhu-test-gdm3-perf-overhead {
     zhu-test-viewperf-maya-subtest5 | tee /tmp/maya.gdm3.log || return -1
-    zhu-xserver || return -1
+    zhu-test-unigine-heaven | tee /tmp/heaven.gdm3.log || return -1
+    zhu-xserver 
     zhu-test-viewperf-maya-subtest5 | tee /tmp/maya.xserver.log || return -1
-    zhu-gdm3 || return -1
-    sleep 3
+    zhu-test-unigine-heaven | tee /tmp/heaven.xserver.log || return -1
+    zhu-gdm3 
 
     fps1=$(cat /tmp/maya.gdm3.log | grep 'result FPS' | awk '{print $5}')
     fps2=$(cat /tmp/maya.xserver.log | grep 'result FPS' | awk '{print $5}')
-    ratio=$(echo "scale=4; $fps2/$fps1" | bc)
+    ratio1=$(echo "scale=4; $fps2/$fps1" | bc)
+    fps3=$(cat /tmp/heaven.gdm3.log | grep 'FPS' | awk '{print $2}')
+    fps4=$(cat /tmp/heaven.xserver.log | grep 'FPS' | awk '{print $2}')
+    ratio2=$(echo "scale=4; $fps4/$fps3" | bc)
+
     echo 
-    echo "FPS on GDM3: $fps1"
-    echo "FPS on Xserver: $fps2 ($ratio)"
+    echo "VP-Maya's FPS on GDM3: $fps1"
+    echo "VP-Maya's FPS on Xserver: $fps2 ($ratio1)"
+    echo " Heaven's FPS on GDM3: $fps3"
+    echo " Heaven's FPS on Xserver: $fps4 ($ratio2)"
 }
