7773671fefd1d318903c8b72fe8db42ce708280d
wanlizhu <wanlizhu@hotmail.com>
2025-03-29 01:11:12 -0700

    Sat Mar 29 01:11:12 PDT 2025

--- nvbugs/gdm3-perf-overhead.sh
+++ nvbugs/gdm3-perf-overhead.sh
@@ -4,6 +4,7 @@ function zhu-test-gdm3-perf-overhead {
     zhu-test-viewperf-maya-subtest5 | tee /tmp/maya.xserver.log || return -1
     zhu-gdm3 || return -1
     sleep 3
+    echo 
     echo "FPS on GDM3: $(cat /tmp/maya.gdm3.log | grep 'result FPS' | awk '{print $5}')"
     echo "FPS on Xserver: $(cat /tmp/maya.xserver.log | grep 'result FPS' | awk '{print $5}')"
 }
