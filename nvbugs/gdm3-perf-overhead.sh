edef92ab8883cda58f3836e61efa03987768f576
wanlizhu <wanlizhu@hotmail.com>
2025-03-29 01:22:31 -0700

    Sat Mar 29 01:22:31 PDT 2025

--- nvbugs/gdm3-perf-overhead.sh
+++ nvbugs/gdm3-perf-overhead.sh
@@ -4,7 +4,11 @@ function zhu-test-gdm3-perf-overhead {
     zhu-test-viewperf-maya-subtest5 | tee /tmp/maya.xserver.log || return -1
     zhu-gdm3 || return -1
     sleep 3
+
+    fps1=$(cat /tmp/maya.gdm3.log | grep 'result FPS' | awk '{print $5}')
+    fps2=$(cat /tmp/maya.xserver.log | grep 'result FPS' | awk '{print $5}')
+    ratio=$(echo "scale=2; ($fps2-$fps1)/$fps1" | bc)
     echo 
-    echo "FPS on GDM3: $(cat /tmp/maya.gdm3.log | grep 'result FPS' | awk '{print $5}')"
-    echo "FPS on Xserver: $(cat /tmp/maya.xserver.log | grep 'result FPS' | awk '{print $5}')"
+    echo "FPS on GDM3: $fps1"
+    echo "FPS on Xserver: $fps2 ($ratio)"
 }
