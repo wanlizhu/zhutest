4948b27276909ec4ab3a25ef1e7d99127301d74e
wanlizhu <wanlizhu@hotmail.com>
2025-04-01 14:15:19 -0700

    Tue Apr  1 14:15:19 PDT 2025

--- env.sh
+++ env.sh
@@ -2238,7 +2238,7 @@ function zhu-vnc-server-for-physical-display {
 
     zhu-check-xauthority || return -1
     if [[ -z $XAUTHORITY ]]; then
-        auth_args=""
+        auth_args="-auth guess"
     else
         auth_args="-auth $XAUTHORITY"
     fi
