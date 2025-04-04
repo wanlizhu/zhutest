ad4311106e50900cfd52d46536c633a998c0cf04
wanlizhu <wanlizhu@hotmail.com>
2025-04-03 23:30:12 -0700

    Thu Apr  3 23:30:12 PDT 2025

--- env.sh
+++ env.sh
@@ -3113,22 +3113,18 @@ function zhu-digits-max-clocks {
         rsync -ah --progress /mnt/linuxqa/wlueking/n1x-bringup/CPU_fmax_scripts $HOME 
     fi
 
-    rm_power_feature=$(cat /proc/driver/nvidia/params | grep RmPowerFeature | awk '{print $2}')
-    rm_power_feature2=$(cat /proc/driver/nvidia/params | grep RmPowerFeature2 | awk '{print $2}')
-    if [[ -z $rm_power_feature || -z $rm_power_feature ]]; then
-        if [[ ! -z $(sudo fuser -v /dev/nvidia*) ]]; then
-            sudo fuser -v /dev/nvidia*
-            read -e -i yes -p "Kill above process? (yes/no): " killproc
-            if [[ $killproc == yes ]]; then
-                for pid in $(sudo fuser -v /dev/nvidia* | grep -v 'COMMAND' | awk '{print $3}' | sort  | uniq); do 
-                    sudo kill -9 $pid 
-                done
-            fi
-            sleep 1
+    if [[ ! -z $(sudo fuser -v /dev/nvidia*) ]]; then
+        sudo fuser -v /dev/nvidia*
+        read -e -i yes -p "Kill above process? (yes/no): " killproc
+        if [[ $killproc == yes ]]; then
+            for pid in $(sudo fuser -v /dev/nvidia* | grep -v 'COMMAND' | awk '{print $3}' | sort  | uniq); do 
+                sudo kill -9 $pid 
+            done
         fi
-        sudo modprobe nvidia NVreg_RegistryDwords="RmPowerFeature=0x55455555; RmPowerFeature2=0x55555550;"
-    fi 
+        sleep 1
+    fi
 
+    sudo modprobe nvidia NVreg_RegistryDwords="RmPowerFeature=0x55455555; RmPowerFeature2=0x55555550;"
     cat /proc/driver/nvidia/params | grep RmPowerFeature
     cat /proc/driver/nvidia/params | grep RmPowerFeature2 
 
